02eaf48647f2e59cff9e7870efaabb002bd23997
Zsolt <zsolt.sandor@gmail.com>
2021-04-25 00:20:41 +0200

    Added ability to add a new certificate + powershell script for displaying the data

--- minimal-crypto-example.js
+++ minimal-crypto-example.js
@@ -8,8 +8,11 @@
 
 const ffi = require('ffi-napi');
 const ref = require('ref-napi');
+const wchar_t = require('ref-wchar-napi');
 const StructType = require('ref-struct-di')(ref);
 const fs = require('fs');
+
+const wchar_string = wchar_t.string;
 
 // definitions and constants are from the following header file
 //
@@ -19,10 +22,15 @@ const fs = require('fs');
 
 const X509_ASN_ENCODING = 0x00000001;
 const PKCS_7_ASN_ENCODING = 0x00010000;
+const PROV_RSA_FULL = 1;
+const AT_KEYEXCHANGE = 1;
+const CERT_KEY_PROV_INFO_PROP_ID = 2;
+const CERT_STORE_ADD_REPLACE_EXISTING = 3;
 
 const HCERTSTORE = 'void *';
 const DWORD = ref.types.ulong;
 const BYTE_PTR = ref.refType(ref.types.uint8);
+const LPWSTR = wchar_string;
 
 const _CRYPTOAPI_BLOB = StructType({
     cbData: DWORD,
@@ -88,6 +96,26 @@ const _CERT_CONTEXT = StructType({
 });
 
 const PCCERT_CONTEXT = ref.refType(_CERT_CONTEXT);
+const PPCCERT_CONTEXT = ref.refType(PCCERT_CONTEXT);
+
+const _CRYPT_KEY_PROV_PARAM = StructType({
+    dwParam: DWORD,
+    pbData: BYTE_PTR,
+    cbData: DWORD,
+    dwFlags: DWORD
+});
+
+const PCRYPT_KEY_PROV_PARAM = ref.refType(_CRYPT_KEY_PROV_PARAM);
+
+const _CRYPT_KEY_PROV_INFO = StructType({
+    pwszContainerName: LPWSTR,        // LPWSTR
+    pwszProvName: LPWSTR,             // LPWSTR
+    dwProvType: DWORD,
+    dwFlags: DWORD,
+    cProvParam: DWORD,
+    rgProvParam: PCRYPT_KEY_PROV_PARAM,
+    dwKeySpec: DWORD
+});
 
 // -------------------------------------------------------------------------------------
 
@@ -101,38 +129,38 @@ const libcrypto = ffi.Library('C:/windows/system32/crypt32.dll', {
     'CertOpenSystemStoreA': [HCERTSTORE, ['int', 'string']],
     'CertCloseStore': ['bool', [HCERTSTORE, 'int']],
     'CertCreateCertificateContext': [PCCERT_CONTEXT, [DWORD, BYTE_PTR, DWORD]],
-    //'CertSetCertificateContextProperty': ['',[]],
-    //'CertAddCertificateContextToStore': ['',[]]
+    'CertSetCertificateContextProperty': ['bool', [PCCERT_CONTEXT, DWORD, DWORD, 'void*']],
+    'CertAddCertificateContextToStore': ['bool', [HCERTSTORE, PCCERT_CONTEXT, DWORD, PPCCERT_CONTEXT]]
 });
 
 const knl32 = ffi.Library('kernel32.dll', {
     GetLastError: ['uint32', []],
     FormatMessageW: [
-      'uint',
-      ['uint', 'pointer', 'uint', 'uint', 'pointer', 'uint', 'pointer'],
+        'uint',
+        ['uint', 'pointer', 'uint', 'uint', 'pointer', 'uint', 'pointer'],
     ],
-  })
+})
 
 function get_last_err_msg() {
     const errcode = knl32.GetLastError()
     let errMsg = ''
-  
+
     /* istanbul ignore if  */
     if (errcode) {
-      const len = 255
-      const buf = Buffer.alloc(len)
-      // tslint:disable-next-line:no-bitwise
-      const p = 0x00001000 | 0x00000200  // FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS
-      const langid = 0x0409              // 0x0409: US, 0x0000: Neutral locale language
-      const msglen = knl32.FormatMessageW(p, null, errcode, langid, buf, len, null)
-  
-      errMsg = msglen
-        ? errcode + ': ' + ref.reinterpretUntilZeros(buf, 2).toString('ucs2')
-        : `${errcode}: unknown error message`
+        const len = 255
+        const buf = Buffer.alloc(len)
+        // tslint:disable-next-line:no-bitwise
+        const p = 0x00001000 | 0x00000200  // FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS
+        const langid = 0x0409              // 0x0409: US, 0x0000: Neutral locale language
+        const msglen = knl32.FormatMessageW(p, null, errcode, langid, buf, len, null)
+
+        errMsg = msglen
+            ? errcode + ': ' + ref.reinterpretUntilZeros(buf, 2).toString('ucs2')
+            : `${errcode}: unknown error message`
     }
-  
+
     return errMsg
-  }
+}
 
 
 // ------------------------------------------------------------------------
@@ -141,7 +169,7 @@ console.log('Trying to open certificate store');
 
 const hSystemStore = libcrypto.CertOpenSystemStoreA(ref.NULL, 'MY')
 
-if (ref.isNull(hSystemStore)){
+if (ref.isNull(hSystemStore)) {
     console.error('Cannot open store');
 }
 
@@ -167,6 +195,23 @@ if (ref.isNull(pCertContext)) {
 
 // ------------------------------------------------------------------------
 
+const keyinfo = new _CRYPT_KEY_PROV_INFO();
+
+// FIXME the following two strings shall be using 16 bit unicode characters instead of utf8 or whatever
+keyinfo.pwszProvName = 'MY Key Storage Provider';
+keyinfo.pwszContainerName = 'my_id my_api_key';       // here comes the id and the api_key
+keyinfo.dwProvType = PROV_RSA_FULL;
+keyinfo.dwKeySpec = AT_KEYEXCHANGE;
+
+const keyinfoBuffer = keyinfo.ref();
+
+if (!libcrypto.CertSetCertificateContextProperty(pCertContext, CERT_KEY_PROV_INFO_PROP_ID, 0, keyinfoBuffer)) {
+    console.error('Cannot set cert context property, reason = ' + get_last_err_msg());
+}
+
+if (!libcrypto.CertAddCertificateContextToStore(hSystemStore, pCertContext, CERT_STORE_ADD_REPLACE_EXISTING, ref.NULL)) {
+    console.error('Cannot add certificate to store, reason = ' + get_last_err_msg());
+}
 
 if (!libcrypto.CertCloseStore(hSystemStore, ref.NULL)) {
     console.log("Unable to close the  system store.\n");
